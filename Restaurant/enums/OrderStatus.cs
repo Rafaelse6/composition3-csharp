aa5e8bb62d24c964bafb39867fafab7bca5defe2
Rafael Santos <rafa.05125@gmail.com>
2023-06-11 22:43:53 -0300

    Fix: Processing enum

--- Restaurant/enums/OrderStatus.cs
+++ Restaurant/enums/OrderStatus.cs
@@ -3,7 +3,7 @@ namespace Restaurant.Entities.Enums
     enum OrderStatus : int
     {
         PendingPayment = 0,
-        Processig = 1,
+        Processing = 1,
         Shipped = 2,
         Delivered = 3
     }
