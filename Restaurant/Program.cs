ea498c5e2e8fa169f2447cf58216504b4d25e0eb
Rafael Santos <rafa.05125@gmail.com>
2023-06-11 22:45:52 -0300

    Final implementation

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace Restaurant
+﻿using Restaurant.Entities;
+using System.Globalization;
+using Restaurant.Entities.Enums;
+
+namespace Restaurant
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
 
+            Console.Write("Email: ");
+            string clientEmail = Console.ReadLine();
+
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime clientBirthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(clientName, clientEmail, clientBirthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string produtName = Console.ReadLine();
+
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(produtName, price);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(quantity, price, product);
+                order.AddItem(orderItem);
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
         }
     }
 }
