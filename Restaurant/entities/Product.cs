5bad220da78d302e35698d450503157cec53a719
Rafael Santos <rafa.05125@gmail.com>
2023-06-11 22:06:29 -0300

    Product

---
+++ Restaurant/entities/Product.cs
@@ -0,0 +1,16 @@
+namespace Restaurant.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
